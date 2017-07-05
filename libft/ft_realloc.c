2200964a97763ba9f8f93b14dfcafeb32bf9046b
DUNCAN LEMP <marialemp@MARIAs-MacBook-Air-2.local>
2017-07-05 15:49:06 -0700

    added ft_realloc.c

---
+++ libft/ft_realloc.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <stdlib.h>
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	char	*newptr;
+
+	if (!size && ptr)
+	{
+		if (!(newptr = (char *)malloc(1)))
+			return (NULL);
+		ft_memdel(&ptr);
+		return (newptr);
+	}
+	if (!(newptr = (char *)malloc(size)))
+		return (NULL);
+	if (ptr)
+	{
+		ft_memcpy(newptr, ptr, size);
+		ft_memdel(&ptr);
+	}
+	return (newptr);
+}
